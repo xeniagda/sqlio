ac54db83fc3638afbd66d6dba6be6ef421658eda
xenia <xenia.agda@gmail.com>
2024-09-20 23:46:19 +0200

    Show headers in http server

--- http-server.sql
+++ http-server.sql
@@ -16,11 +16,16 @@ BEGIN
     INSERT INTO got_byte (token, byte) VALUES (NEW.token, char(NEW.byte));
 END;
 
-CREATE TRIGGER message_parsed AFTER INSERT ON processed_requests
+CREATE TRIGGER get_index AFTER INSERT ON processed_requests
+    WHEN EXISTS (SELECT * FROM parsed_requests WHERE token = NEW.token AND method = 'GET' AND (path = '/index.html' OR path = '/'))
 BEGIN
     WITH
         req(method, path, http_version, body) AS (SELECT method, path, http_version, body FROM parsed_requests WHERE token = NEW.token),
-        html(data) AS (SELECT '<html><body><h1> welcome to sqlsite! </h1> <p> you requested ' || method || ' ' || path || '</body></html>' FROM req),
+        html(data) AS (
+            SELECT '<html><body><h1> Welcome to SQLSite! </h1> <h3> Your headers: </h3>' || (
+                SELECT group_concat('<b>' || header_key || '</b>: ' || header_value, '</br>')
+                    FROM parsed_request_headers WHERE token = NEW.token
+            ) || '</body></html>' FROM req),
         header_lines(line) AS (VALUES('HTTP/1.1 200 Ok'),('Content-Length: ' || (SELECT length(data) FROM html)),('')),
         header(data) AS (SELECT group_concat(line, char(13) || char(10)) FROM header_lines),
         response(data) AS (SELECT group_concat(data, char(13) || char(10)) FROM (SELECT data FROM header UNION ALL SELECT data FROM html))
@@ -28,5 +33,3 @@ BEGIN
 END;
 
 SELECT tcp_listen('0.0.0.0:1234', 'connect_cb', 'data_cb');
-SELECT write_to_file('/dev/stdout', 'listening on localhost:1234');
-
